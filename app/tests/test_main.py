df2627ed9d8c29a6bfca76530ce194d0ad0cd966
Evaristo Ferreira <evaristo.ferreira@propig.com.br>
2023-07-26 09:48:51 -0300

    Base

---
+++ app/tests/test_main.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+client = TestClient(app)
+
+
+def test_main_resource():
+    response_auth = client.get("/")
+    assert response_auth.status_code == 200
+
+
+def test_child_resource():
+    response_auth = client.get("/api/v1/test")
+    assert response_auth.status_code == 200
