2739cd3cfa9769db2317aa0c1a23be3c6cbade20
Evaristo Ferreira <evaristo.ferreira@propig.com.br>
2023-07-26 11:22:24 -0300

    add confi CORS

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
 from api.v1.api import router as api_router
 
 app = FastAPI(title='Serverless Lambda FastAPI')
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/",  tags=["Endpoint Test"])
 def main_endpoint_test():
     return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
