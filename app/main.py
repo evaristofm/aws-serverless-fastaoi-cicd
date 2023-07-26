e10bbf04af153d52fb065045ab596e8a4a3230a4
Evaristo Ferreira <evaristo.ferreira@propig.com.br>
2023-07-26 10:02:15 -0300

    Movendo arquivo de test

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from api.v1.api import router as api_router
 
 app = FastAPI(title='Serverless Lambda FastAPI')
 
-app.include_router(api_router, prefix="/api/v1")
+
 
 
 @app.get("/",  tags=["Endpoint Test"])
@@ -13,5 +13,6 @@ def main_endpoint_test():
     return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
 
 
+app.include_router(api_router, prefix="/api/v1")
 # to make it work with Amazon Lambda, we create a handler object
 handler = Mangum(app=app)
